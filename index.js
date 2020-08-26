3735516ed55a26d3d48d11091f006b8ee19a7cdb
Steve Sanchez <superheromanman@gmail.com>
2020-08-26 16:12:31 -0400

    applied cors policy

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var static = require('node-static');
 var http = require('http');
 
-var file = new(static.Server)('./public');
+
+var file = new static.Server({
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET',
+      'Access-Control-Allow-Headers': 'Content-Type'
+    },
+    indexFile: "./public/index.html"
+  });
+
 
     http.createServer(function (req, res) {
         file.serve(req, res);
