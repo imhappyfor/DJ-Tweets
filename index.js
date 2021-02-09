d1bc7335dc428ff254d1ecef674c5e448ade5098
imhappyfor <superheromanman@gmail.com>
2021-02-09 14:20:58 -0500

    asdfasdf

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ var cors = require('cors');
 var path = require('path');
 app.use(cors());
 const port = 3000
-app.use('/public', express.static('public/'))
+// app.use('/public', express.static('public/'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
 
 // app.get('/melodies', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/melodyexperimenting.html'));
+    // res.sendFile(path.join(__dirname + '/melodyexperimenting.html'));
 // })
 
-app.listen(port, () => {
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`)
+// })
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+
